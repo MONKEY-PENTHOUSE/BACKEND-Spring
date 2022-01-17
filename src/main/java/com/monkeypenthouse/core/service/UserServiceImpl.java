950730bdc222c487b04821c529b178170c86ea93
Hee-jin506 <59636609+Hee-jin506@users.noreply.github.com>
2022-01-17 23:01:20 +0900

    add : delete LocalLoginFailedException, setHideUsernameNotFoundException false

--- src/main/java/com/monkeypenthouse/core/service/UserServiceImpl.java
+++ src/main/java/com/monkeypenthouse/core/service/UserServiceImpl.java
@@ -18,8 +18,8 @@ import lombok.extern.log4j.Log4j2;
 
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.*;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
@@ -112,9 +112,6 @@ public class UserServiceImpl implements UserService {
     @Transactional
     public Map<String, Object> login(User user) throws AuthenticationException, AuthFailedException {
 
-        userRepository.findByEmail(user.getEmail())
-                .orElseThrow(() -> new UsernameNotFoundException("해당 이메일을 가진 회원이 존재하지 않습니다."));
-
         // 1. Login ID/PW를 기반으로 authenticationToken 생성
         UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(user.getEmail(), user.getPassword());
 
@@ -122,7 +119,16 @@ public class UserServiceImpl implements UserService {
         // 2. 실제로 검증이 이뤄지는 부분
         // authentication 메서드가 실행이 될 때 CustomUserDetailService에서 만들었던 loadUserByUser
         try {
-            authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+            ProviderManager providerManager = (ProviderManager) authenticationManagerBuilder.getObject();
+
+            for (AuthenticationProvider provider : providerManager.getProviders()) {
+                if (provider instanceof DaoAuthenticationProvider) {
+                    ((DaoAuthenticationProvider) provider).setHideUserNotFoundExceptions(false);
+                }
+            }
+
+            authentication = providerManager.authenticate(authenticationToken);
+
         } catch (BadCredentialsException e) {
             throw new BadCredentialsException("비밀번호가 일치하지 않습니다.");
         }
