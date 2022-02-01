35dfe88398b504ea8c97b9667fcdf69db08890d7
Hee-jin506 <59636609+Hee-jin506@users.noreply.github.com>
2022-02-02 02:45:00 +0900

    add: test report

--- scripts/afterInstall.bash
+++ scripts/afterInstall.bash
@@ -1,2 +1,2 @@
 docker pull monkeypenthouse/api-server-spring:v1
-docker run -e spring.profiles.active=prod --publish 80:80 -i --detach --name api-server-spring monkeypenthouse/api-server-sprin
+docker run -e spring.profiles.active=prod --publish 80:80 -i --detach --name api-server-spring monkeypenthouse/api-server-spring
