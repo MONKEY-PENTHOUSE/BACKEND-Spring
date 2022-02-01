376b83b31207609173a2497b3b60b5d9de80e2e5
Hee-jin506 <59636609+Hee-jin506@users.noreply.github.com>
2022-02-02 01:35:36 +0900

    fix: change ddl-auto options

--- src/test/java/com/monkeypenthouse/core/CoreApplicationTests.java
+++ src/test/java/com/monkeypenthouse/core/CoreApplicationTests.java
@@ -1,14 +1,21 @@
 package com.monkeypenthouse.core;
 
+import com.monkeypenthouse.core.service.UserService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.MatcherAssert.*;
+import static org.hamcrest.CoreMatchers.*;
 
 @SpringBootTest
 class CoreApplicationTests {
 
+	@Autowired
+	UserService userService;
+
 	@Test
 	void contextLoads() {
-
+		assertThat(userService.checkEmailDuplicate("gmlwls3520@naver.com"), is(false));
 	}
 
 }
