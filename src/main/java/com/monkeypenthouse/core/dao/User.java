8bc7d9c0c2ba7fe43369bc168fec24bc9989dfee
Hee-jin506 <59636609+Hee-jin506@users.noreply.github.com>
2022-02-23 04:17:41 +0900

    Update User.java

--- src/main/java/com/monkeypenthouse/core/dao/User.java
+++ src/main/java/com/monkeypenthouse/core/dao/User.java
@@ -4,6 +4,8 @@ import lombok.*;
 
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
@@ -31,7 +33,7 @@ public class User {
     @Column(name="created_at", updatable=false, nullable=false)
     private LocalDateTime createdDateTime;
 
-    @UpdateTimestamp
+    @LastModifiedDate
     @Column(name="last_modified_at")
     private LocalDateTime lastModifiedDateTime;
 
