1298164bd1c6b10227b92bab22908e079258d422
Hee-jin506 <59636609+Hee-jin506@users.noreply.github.com>
2022-03-01 19:13:16 +0900

    small fix : afterInstall.bash local.properties

--- scripts/afterInstall.bash
+++ scripts/afterInstall.bash
@@ -2,4 +2,4 @@ docker pull monkeypenthouse/api-server-spring:v1
 docker create -e spring.profiles.active=prod --publish 80:80 --name api-server-spring -i monkeypenthouse/api-server-spring:v1
 docker cp /home/ubuntu/resources/key-pair.der api-server-spring:/
 docker cp /home/ubuntu/resources/application-prod.properties api-server-spring:/
-docker run --detach api-server-spring
+docker run --detach monkeypenthouse/api-server-spring:v1 --name api-server-spring -rm api-server-spring
