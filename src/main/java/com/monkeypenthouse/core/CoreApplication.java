44487860e378b8e1db5091739ed55941c290c36f
seopjikoji <ctensoreo@gmail.com>
2022-03-14 18:18:22 +0900

    test commit

--- src/main/java/com/monkeypenthouse/core/CoreApplication.java
+++ src/main/java/com/monkeypenthouse/core/CoreApplication.java
@@ -3,6 +3,7 @@ package com.monkeypenthouse.core;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
