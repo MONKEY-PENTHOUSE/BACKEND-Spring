445877577dc93b84c3ac97634974261f6db930fd
seungwookim <seungwookim.dev@gmail.com>
2022-06-22 18:34:22 +0900

    add : CancelReason, Purchase from feature/111

--- src/main/java/com/monkeypenthouse/core/repository/entity/Purchase.java
+++ src/main/java/com/monkeypenthouse/core/repository/entity/Purchase.java
@@ -2,11 +2,13 @@ package com.monkeypenthouse.core.repository.entity;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Getter
@@ -40,6 +42,17 @@ public class Purchase {
     @Column(name="created_at", updatable=false, nullable=false)
     private LocalDateTime createdAt;
 
+    @Column(name="payments_key", updatable = false, nullable = false)
+    private String paymentsKey;
+
+    @Enumerated(EnumType.STRING)
+    @Column(name="cancel_reason")
+    private CancelReason cancelReason;
+
+    @OneToMany(mappedBy = "amenity")
+    @ToString.Exclude
+    private List<PurchaseTicketMapping> purchaseTicketMappingList;
+
     public Purchase(User user, String orderId, String orderName, int amount, OrderStatus orderStatus) {
         this.user = user;
         this.orderId = orderId;
@@ -51,4 +64,7 @@ public class Purchase {
     public void changeOrderStatus(OrderStatus orderStatus) {
         this.orderStatus = orderStatus;
     }
+
+    public void setCancelReason(CancelReason cancelReason) { this.cancelReason = cancelReason; }
+    public void setPaymentsKey(String paymentsKey) { this.paymentsKey = paymentsKey; }
 }
